03bf7be3227b1b0050f4fece95b9f7939aae3e29
ntokozo-sithebe <ntoky28@gmail.com>
2023-11-13 13:31:49 +0200

    challengeEccepted
    git commit -m challengeEccepted

---
+++ script/code.js
@@ -0,0 +1,28 @@
+
+let marks = 90
+
+switch (marks){
+    case 100 :
+        console.log("A+")
+break;
+    case 89 > 90 < 99:
+        console.log("A")
+break;
+        case 80 >= 89:
+            console.log("B")
+break; 
+       case 70 >= 79:
+       console.log("Distinction")
+
+break;
+       case 60>=69 : 
+       console.log("C+")
+
+    break;
+    case 50 >= 59 :
+        console.log("Passed")
+        break;
+    case 49 <= 50 :
+        console.log("Failed")
+
+}
